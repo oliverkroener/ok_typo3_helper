8b6a91bf4b7228d8dd56b7dd63d5969d5e580e35
Oliver Kroener <ok@oliver-kroener.de>
2024-09-15 06:31:21 +0000

    [TASK] Adjusted compatibility to Typo3 9 & 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Helpers',
+    'title' => 'A TYPO3 extension with helper traits and utilities.',
     'description' => 'A TYPO3 extension with helper traits and utilities.',
     'category' => 'plugin',
     'author' => 'Oliver Kroener',
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
