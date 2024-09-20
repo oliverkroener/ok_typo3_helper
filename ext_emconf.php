29307b752b354a22f0c8c193b62c63adf3cbfd08
Oliver Kroener <ok@oliver-kroener.de>
2024-09-20 07:42:52 +0000

    [TASK] Changed dependencies for Typo3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
