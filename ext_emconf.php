5b7bbc4a020c890e056e125cb7a748cf00fd5913
Oliver Kroener <ok@oliver-kroener>
2026-01-21 19:23:54 +0100

    [TASK] Updated version and Typo3 14 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,10 +6,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Oliver Kroener',
     'author_email' => 'ok@oliver-kroener.de',
     'state' => 'stable',
-    'version' => '3.0.6',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
